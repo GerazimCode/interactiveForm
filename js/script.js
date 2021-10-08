9c8e56fcc217b52970ca3fe805c8957d7d22c3c7
GerazimCode <gerazimcode@gmail.com>
2021-10-08 11:43:21 -0400

    add other job hide functionality

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@
 let nameField = document.getElementById("name");
 nameField.focus();
 
+// following code hides the other job roles and shows it only when other is selected
+
+let jobRole = document.querySelector("#title");
+let otherJob = document.querySelector("#other-job-role");
+// hides the other job element by default upon reload
+otherJob.style.display = "none";
+
+jobRole.addEventListener("change", (e)=>{
+    let target = e.target;
+    if(target.value === "other"){
+        otherJob.style.display = "block";
+    }else{
+        otherJob.style.display = "none";
+    }
+})
